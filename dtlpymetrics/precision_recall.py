fixed precision recall with curve function

dtlpymetrics/precision_recall.py
@@ -1,4 +1,6 @@
 import os
+from typing import Dict, Union, List, Any
+
 import numpy as np
 import pandas as pd
 import dtlpy as dl
@@ -9,23 +11,18 @@ from dtlpymetrics.tools.utils import MethodAveragePrecision
 
 def calc_precision_recall(dataset_id: str,
                           model_id: str,
-                          metric: str,
-                          metric_threshold=0.5,
-                          method_type='every_point'):
+                          conf_threshold=0.5,
+                          iou_threshold=0.5):
+    # method_type='every_point'):
     """
     Plot precision recall curve for a given metric threshold
 
-    :param dataset_id: dataset ID
-    :param model_id: model ID
-    :param metric: name of the column in the scores dataframe to use as the metric
-    :param metric_threshold: threshold for which to calculate TP/FP
-    :param method_type: 'every_point' or 'eleven_point'
+    :param dataset_id: str dataset ID
+    :param model_id: str model ID
+    :param conf_threshold: 
+    :param iou_threshold:
     :return:
     """
-    if metric.lower() == 'iou':
-        metric = 'geometry_score'
-    elif metric.lower() == 'accuracy':
-        metric = 'label_score'
 
     model_filename = f'{model_id}.csv'
     filters = dl.Filters(field='hidden', values=True)
@@ -43,53 +40,89 @@ def calc_precision_recall(dataset_id: str,
     labels = dataset.labels
     label_names = [label.tag for label in labels]
 
-    if metric not in scores.columns:
-        raise ValueError(f'{metric} metric not included in scores.')
-
-    #########################
-    # plot precision/recall #
-    #########################
+    ##############################
+    # calculate precision/recall #
+    #############################
     # calc
     if labels is None:
         labels = pd.concat([scores.first_label, scores.second_label]).dropna()
 
-    plot_points = {'metric': metric,
-                   'metric_threshold': metric_threshold,
-                   'method_type': method_type,
-                   'labels': {}}
+    plot_points = {'conf_threshold': conf_threshold,
+                   'iou_threshold': iou_threshold,
+                   'labels': {},
+                   'dataset_precision': [],
+                   'dataset_recall': []
+                   }
 
+    num_gts = sum(scores.first_id.notna())
+
+    scores_positives = scores[scores['geometry_score'] > iou_threshold]
+
+    scores_positives.sort_values('second_confidence', inplace=True, ascending=True, ignore_index=True)
+    scores_positives['true_positives'] = scores_positives['second_confidence'] >= conf_threshold
+    scores_positives['false_positives'] = scores_positives['second_confidence'] < conf_threshold
+
+    # get dataset-level precision/recall
+    dataset_fps = np.cumsum(scores_positives['false_positives'])
+    dataset_tps = np.cumsum(scores_positives['true_positives'])
+    dataset_recall = dataset_tps / num_gts
+    dataset_precision = np.divide(dataset_tps, (dataset_fps + dataset_tps))
+
+    [_, dataset_plot_precision, dataset_plot_recall] = every_point_curve(dataset_recall, dataset_precision)
+    plot_points['dataset_precision'] = dataset_plot_precision
+    plot_points['dataset_recall'] = dataset_plot_recall
+
+    # get label-level precision/recall
     for label in list(set(label_names)):
-        label_confidence_df = scores[scores.first_label == label].copy()
+        label_positives = scores_positives[scores_positives.first_label == label].copy()
+        label_positives.sort_values('second_confidence', inplace=True, ascending=True, ignore_index=True)
 
-        label_confidence_df.sort_values('second_confidence', inplace=True, ascending=True)
-        true_positives = label_confidence_df[metric] >= metric_threshold
-        false_positives = label_confidence_df[metric] < metric_threshold
-        #
-        num_gts = sum(scores.first_id.notna())
+        label_fps = np.cumsum(label_positives['false_positives'])
+        label_tps = np.cumsum(label_positives['true_positives'])
+        label_recall = label_tps / num_gts
+        label_precision = np.divide(label_tps, (label_fps + label_tps))
 
-        #
-        acc_fps = np.cumsum(false_positives)
-        acc_tps = np.cumsum(true_positives)
-        recall = acc_tps / num_gts
-        precision = np.divide(acc_tps, (acc_fps + acc_tps))
-
-        # # Depending on the method, call the right implementation
-        if method_type == 'every_point':
-            [avg_precis, mpre, mrec, ii] = Evaluator.CalculateAveragePrecision(recall, precision)
-        else:
-            [avg_precis, mpre, mrec, _] = Evaluator.ElevenPointInterpolatedAP(recall, precision)
+        [_, label_plot_precision, label_plot_recall] = every_point_curve(label_recall, label_precision)
 
         plot_points['labels'].update({label: {
-            'precision': mpre,
-            'recall': mrec}})
+            'precision': label_plot_precision,
+            'recall': label_plot_recall}})
 
     return plot_points
 
 
-def calc_confusion_matrix(dataset_id,
-                          model_id,
-                          metric,
-                          show_unmatched=False):
+def every_point_curve(recall: list, precision: list):
+    """
+    Calculate precision-recall curve from a list of precision & recall values
+    :param recall: list of recall values
+    :param precision: list of precision values
+    :return:
+    """
+    recall_points = np.concatenate([[0], recall, [1]])
+    precision_points = np.concatenate([[0], precision, [1]])
+
+    # find the maximum precision between each recall value, backwards
+    for i in range(len(precision_points) - 1, 0, -1):
+        precision_points[i - 1] = max(precision_points[i - 1], precision_points[i])
+
+    # build the simplified recall list, removing values when the precision doesnt change
+    recall_intervals = []
+    for i in range(len(recall_points) - 1):
+        if recall_points[1 + i] != recall_points[i]:
+            recall_intervals.append(i + 1)
+
+    avg_precis = 0
+    for i in recall_intervals:
+        avg_precis = avg_precis + np.sum((recall_points[i] - recall_points[i - 1]) * precision_points[i])
+    return [avg_precis,
+            precision_points[0:len(precision_points) - 1],
+            recall_points[0:len(precision_points) - 1]]
+
+
+def calc_confusion_matrix(dataset_id: str,
+                          model_id: str,
+                          metric: str,
+                          show_unmatched=True):
     """
     Calculate confusion matrix for a given model and metric
     :param dataset_id:
@@ -175,14 +208,14 @@ if __name__ == '__main__':
 
     dataset_id = '64731b043e2dd675c25cce88'  # big cats TEST evaluate
     model_id = '6473185c93bd97c6a30a47b9'  # resnet
-    metric = 'accuracy'
+    # model_id = '' # yolov8
 
-    # plot_points = calc_precision_recall(dataset_id=dataset_id,
-    #                                     model_id=model_id,
-    #                                     metric=metric,
-    #                                     metric_threshold=0.5,
-    #                                     method_type='every_point')
-    # plot_precision_recall(plot_points)
+    plot_points = calc_precision_recall(dataset_id=dataset_id,
+                                        model_id=model_id,
+                                        conf_threshold=0.5)
+    plot_precision_recall(plot_points)
+
+    metric = 'accuracy'
     conf_table = calc_confusion_matrix(dataset_id=dataset_id,
                                        model_id=model_id,
                                        metric=metric)