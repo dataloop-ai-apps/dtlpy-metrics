9afc13771e74ff265967dbd0d673f2918a41febc
Yaya <tang.yaya@gmail.com>
2024-12-19 11:30:39 +0200

    fix tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,3 @@
-import shutil
-import tempfile
 import unittest
 import datetime
 import logging
@@ -32,35 +30,30 @@ class TestRunner(unittest.TestCase):
         logger.info('[SETUP] - done getting entities')
         now = datetime.datetime.now().isoformat(sep='.', timespec='minutes').replace('.', '_').replace(':', '.')
         self.assets_path = os.path.join(PATH, 'assets')
-        self.test_dump_path = os.path.join(PATH, 'assets', now)
-        os.makedirs(self.test_dump_path, exist_ok=True)
-        logger.info(f'Temporary directory created at: {self.test_dump_path}')
 
     def tearDown(self) -> None:
-        if os.path.isdir(self.test_dump_path):
-            shutil.rmtree(self.test_dump_path)
+        pass
 
     def test_qualification_task(self):
         logger.info(f'Starting qualification testing task with dataset: {self.qualification_task.dataset}')
         qualification_scores = calc_task_score(task=self.qualification_task,
-                                                  score_types=[ScoreType.ANNOTATION_LABEL,
-                                                               ScoreType.ANNOTATION_IOU],
-                                                  upload=False)
+                                               score_types=[ScoreType.ANNOTATION_LABEL,
+                                                            ScoreType.ANNOTATION_IOU],
+                                               upload=False)
 
         qualification_items = self.qualification_task.get_items().all()
         for item in qualification_items:
             logger.info(f'Comparing calculated scores with reference scores for item: {item.id}')
             with open(os.path.join(self.assets_path, self.qualification_task.id, f'{item.id}.json'), 'r') as f:
                 ref_scores = json.load(f)
-            test_scores = qualification_scores[item.id]
+            test_scores = [score.to_json() for score in qualification_scores[item.id]]
             self.assertListEqual(test_scores, ref_scores)
 
     def test_honeypot_task(self):
         logger.info(f'Starting honeypot testing task with dataset: {self.honeypot_task.dataset}')
-        self.honeypot_task = calc_task_score(task=self.honeypot_task,
-                                             score_types=[ScoreType.ANNOTATION_LABEL],
-                                             upload=False,
-                                             save_dir=self.test_dump_path)
+        honeypot_scores = calc_task_score(task=self.honeypot_task,
+                                          score_types=[ScoreType.ANNOTATION_LABEL],
+                                          upload=False)
 
         filters = dl.Filters()
         filters.add(field='hidden', values=True)
@@ -69,8 +62,7 @@ class TestRunner(unittest.TestCase):
             logger.info(f'Comparing calculated scores with reference scores for item: {item.id}')
             with open(os.path.join(self.assets_path, self.honeypot_task.id, f'{item.id}.json'), 'r') as f:
                 ref_scores = json.load(f)
-            with open(os.path.join(self.test_dump_path, self.honeypot_task.id, f'{item.id}.json'), 'r') as f:
-                test_scores = json.load(f)
+            test_scores = [score.to_json() for score in honeypot_scores[item.id]]
             self.assertListEqual(test_scores, ref_scores)
 
     def test_consensus_tasks(self):
@@ -80,26 +72,26 @@ class TestRunner(unittest.TestCase):
         ###########################
         logger.info('calculating scores for consensus classification task')
         consensus_classification_scores = calc_task_score(task=self.consensus_task_classification,
-                                                             score_types=[ScoreType.ANNOTATION_LABEL],
-                                                             upload=False)
+                                                          score_types=[ScoreType.ANNOTATION_LABEL],
+                                                          upload=False)
 
         consensus_assignment = self.consensus_task_classification.metadata['system']['consensusAssignmentId']
         consensus_class_items = self.consensus_task_classification.get_items(get_consensus_items=True).all()
 
         num_consensus = 0
         for item in consensus_class_items:
-            orig_consensus = False
+            consensus_task_found = False
             for ref_obj in item.metadata['system']['refs']:
                 if ref_obj['id'] == consensus_assignment:
-                    orig_consensus = True
+                    consensus_task_found = True
                     break
-            if orig_consensus is True:
+            if consensus_task_found is True:
                 num_consensus += 1
                 logger.info(f'Comparing calculated scores with reference scores for item: {item.id}')
                 with open(os.path.join(self.assets_path, self.consensus_task_classification.id, f'{item.id}.json'),
                           'r') as f:
                     ref_scores = json.load(f)
-                test_scores = consensus_classification_scores[item.id]
+                test_scores = [score.to_json() for score in consensus_classification_scores[item.id]]
                 self.assertListEqual(test_scores, ref_scores)
         logger.info(f'Compared scores for {num_consensus} consensus classification items')
         print(f'Compared scores for {num_consensus} consensus classification items')
@@ -109,25 +101,25 @@ class TestRunner(unittest.TestCase):
         #################
         logger.info('calculating scores for consensus object detection task')
         consensus_bbox_scores = calc_task_score(task=self.consensus_task_bbox,
-                                                   score_types=[ScoreType.ANNOTATION_LABEL,
-                                                                ScoreType.ANNOTATION_IOU],
-                                                   upload=False)
+                                                score_types=[ScoreType.ANNOTATION_LABEL,
+                                                             ScoreType.ANNOTATION_IOU],
+                                                upload=False)
         consensus_assignment = self.consensus_task_bbox.metadata['system']['consensusAssignmentId']
         consensus_bbox_items = self.consensus_task_bbox.get_items(get_consensus_items=True).all()
 
         num_consensus = 0
         for item in consensus_bbox_items:
-            orig_consensus = False
+            consensus_task_found = False
             for ref_obj in item.metadata['system']['refs']:
                 if ref_obj['id'] == consensus_assignment:
-                    orig_consensus = True
+                    consensus_task_found = True
                     break
-            if orig_consensus is True:
+            if consensus_task_found is True:
                 num_consensus += 1
                 logger.info(f'Comparing calculated scores with reference scores for item: {item.id}')
                 with open(os.path.join(self.assets_path, self.consensus_task_bbox.id, f'{item.id}.json'), 'r') as f:
                     ref_scores = json.load(f)
-                test_scores = consensus_bbox_scores[item.id]
+                test_scores = [score.to_json() for score in consensus_bbox_scores[item.id]]
                 self.assertListEqual(test_scores, ref_scores)
         logger.info(f'Compared scores for {num_consensus} consensus items')
         print(f'Compared scores for {num_consensus} consensus bbox items')
