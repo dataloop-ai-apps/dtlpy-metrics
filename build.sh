5e0e50febc585935a83a0d94c9df041eb9f116d9
Yaya <tang.yaya@gmail.com>
2025-05-07 08:40:42 +0300

    build.sh fix

--- build.sh
+++ build.sh
@@ -1,2 +1,11 @@
 pip uninstall -y dtlpymetrics
-cd /tmp/app/pkgs/dtlpy-metrics && /usr/local/bin/python setup.py install
+
+# cd /tmp/app/pkgs/dtlpy-metrics && /usr/local/bin/python setup.py install
+# Change to the package directory
+cd /tmp/app/pkgs/dtlpy-metrics
+
+# Build the wheel
+python setup.py bdist_wheel
+
+# Install the built wheel
+pip install dist/*.whl
